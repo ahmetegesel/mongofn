fd95261f58280230817338f3984e6494db085399
Ahmet Egesel <ahmetegesel@hotmail.com>
2020-07-12 03:54:25 +0200

    feat: closeClient is introduced but needs to be fully tested before release.

---
+++ src/closeClient.js
@@ -0,0 +1,31 @@
+import createClient from './createClient';
+
+/**
+ * Closes given client. Since {@link createClient} is a memoized function,
+ * it accepts the same params that {@link createClient} accepts, so that
+ * it can get the same client to close it.
+ *
+ * @func closeClient
+ * @type function
+ * @since v0.5.0
+ * @param {string} connectionString Connection String.
+ * @param {object} options Optional MongoDB Client settings.
+ * @return {Promise} A void Promise.
+ * @example
+ *
+ *      const { createClient, closeClient } = require('mongofn');
+ *
+ *      const connectionString = '....';
+ *      const options = {};
+ *
+ *      const client = createClient(connectionString, options);
+ *      findBy(client, 'databaseName', 'collectionName', {name: 'some name'})
+ *      .then(console.log);
+ *
+ *      closeClient(connectionString, options);
+ */
+const closeClient = (connectionString, options = undefined) =>
+  createClient(connectionString, options)
+    .then(client => client.close);
+
+export default closeClient;
