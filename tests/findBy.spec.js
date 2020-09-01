2501bdb565440f09b948b616085806af5dea9794
Ahmet Egesel <ahmetegesel@hotmail.com>
2020-09-01 23:53:24 +0200

    test: findBy tests

---
+++ tests/findBy.spec.js
@@ -0,0 +1,43 @@
+import useCollection from '../src/useCollection';
+import findBy from '../src/findBy';
+
+jest.mock(
+  '../src/useCollection',
+  // eslint-disable-next-line no-unused-vars
+  () => jest.fn((client, dbName, collectionName) => {}),
+);
+
+describe('findBy tests', () => {
+  const connectionString = 'connectionString';
+  const dbName = 'dbName';
+  const collectionName = 'collectionName';
+
+  const toArray = jest.fn();
+  const sampleCollection = {
+    find: jest.fn().mockReturnValue({
+      toArray,
+    }),
+  };
+  useCollection.mockResolvedValue(sampleCollection);
+
+  it('should find the data in the collection by given predicate', async () => {
+    const predicate = {};
+
+    const expectedResult = ['test'];
+    toArray.mockReturnValue(expectedResult);
+
+    const result = await findBy(connectionString, dbName, collectionName, predicate);
+
+    expect(result).toBe(expectedResult);
+    expect(sampleCollection.find).toBeCalledWith(predicate);
+  });
+
+  it('should also pass options parameter to collection.find when it is given in predicate parameter', async () => {
+    const options = {};
+    const predicate = {};
+
+    await findBy(connectionString, dbName, collectionName, [predicate, options]);
+
+    expect(sampleCollection.find).toBeCalledWith(predicate, options);
+  });
+});
