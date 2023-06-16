5da0b0d5d0f988ab51a30087f2e3823d45fb7371
mert.kilic <mwert09@gmail.com>
2023-06-16 16:28:41 +0300

    fix: add types
    Added additional types.

--- typings/upsert.d.ts
+++ typings/upsert.d.ts
@@ -5,9 +5,10 @@ type UpsertResult<TSchema extends Document = Document> = InsertOneResult<TSchema
 
 type UpsertDocument<TSchema extends Document = Document> = OptionalUnlessRequiredId<TSchema> | TSchema
 
-declare function upsert<TSchema extends Document = Document>(client: MongoClientLike, databaseName: string, collectionName: string, doc: UpsertDocument): Promise<UpsertResult<TSchema>>;
-declare function upsert<TSchema extends Document = Document>(client: MongoClientLike, databaseName: string, collectionName: string): (doc: UpsertDocument) => Promise<UpsertResult<TSchema>>;
-declare function upsert<TSchema extends Document = Document>(client: MongoClientLike, databaseName: string): (collectionName: string) => (doc: UpsertDocument) => Promise<UpsertResult<TSchema>>;
-declare function upsert<TSchema extends Document = Document>(client: MongoClientLike): (databaseName: string) => (collectionName: string) => (doc: UpsertDocument) => Promise<UpsertResult<TSchema>>;
+export declare function upsert<TSchema extends Document = Document>(client: MongoClientLike, databaseName: string, collectionName: string, doc: UpsertDocument): Promise<UpsertResult<TSchema>>;
+export declare function upsert<TSchema extends Document = Document>(client: MongoClientLike, databaseName: string, collectionName: string): (doc: UpsertDocument) => Promise<UpsertResult<TSchema>>;
+export declare function upsert<TSchema extends Document = Document>(client: MongoClientLike, databaseName: string): (collectionName: string) => (doc: UpsertDocument) => Promise<UpsertResult<TSchema>>;
+export declare function upsert<TSchema extends Document = Document>(client: MongoClientLike, databaseName: string): (collectionName: string, doc: UpsertDocument) => Promise<UpsertResult<TSchema>>;
+export declare function upsert<TSchema extends Document = Document>(client: MongoClientLike): (databaseName: string) => (collectionName: string) => (doc: UpsertDocument) => Promise<UpsertResult<TSchema>>;
 
 export default upsert;
