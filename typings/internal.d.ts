35d99441410009e080a4c228a7a0575ef92b1a34
mert.kilic <mwert09@gmail.com>
2023-07-31 15:01:11 +0300

    refactor: add null return support
    Added null return support to mapResultWith
    function.

--- typings/internal.d.ts
+++ typings/internal.d.ts
@@ -3,12 +3,12 @@ import {Document, WithId} from "mongodb";
 type MongoDocument<TSchema extends { [key: string]: any } = Document> = TSchema | WithId<TSchema>;
 
 export declare function mapResultWith<TSchema extends MongoDocument>(
-  transform: <TItem extends Document = Document> (item: TSchema) => TItem, result: MongoDocument<TSchema> | MongoDocument<TSchema>[]
-): TSchema | TSchema[]
+  transform: <TItem extends Document = Document> (item: TItem | null) => TItem, result: MongoDocument<TSchema> | MongoDocument<TSchema>[]
+): TSchema | TSchema[] | null
 export declare function mapResultWith<TSchema extends MongoDocument>(
-  transform: <TItem extends Document = Document> (item: TSchema) => TItem
-): (result: MongoDocument<TSchema> | MongoDocument<TSchema>[]) => TSchema | TSchema[]
+  transform: <TItem extends Document = Document> (item: TItem | null) => TItem
+): (result: MongoDocument<TSchema> | MongoDocument<TSchema>[]) => TSchema | TSchema[] | null
 
-export declare function toModel<TSchema extends Document = Document>(doc: WithId<TSchema>): TSchema;
+export declare function toModel<TSchema extends Document = Document>(doc: WithId<TSchema>): TSchema | null;
 
-export declare function toDoc<TSchema extends Document = Document>(model: TSchema): WithId<TSchema>;
+export declare function toDoc<TSchema extends Document = Document>(model: TSchema): WithId<TSchema> | null;
