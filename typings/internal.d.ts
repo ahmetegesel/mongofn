2c06706fc4f3add20374151c79f08deb5abaa9a0
mert.kilic <mwert09@gmail.com>
2023-08-02 16:47:53 +0300

    refactor: remove default value
    chore(release): 0.7.24
    
    fix: remove null
    
    Removed null from toDoc function
    Changed changelog

--- typings/internal.d.ts
+++ typings/internal.d.ts
@@ -3,12 +3,12 @@ import {Document, WithId} from "mongodb";
 type MongoDocument<TSchema extends { [key: string]: any } = Document> = TSchema | WithId<TSchema>;
 
 export declare function mapResultWith<TSchema extends MongoDocument>(
-  transform: <TItem extends Document = Document> (item: TItem | null) => TItem, result: MongoDocument<TSchema> | MongoDocument<TSchema>[]
+  transform: <TItem extends Document> (item: TItem | null) => TItem, result: MongoDocument<TSchema> | MongoDocument<TSchema>[]
 ): TSchema | TSchema[] | null
 export declare function mapResultWith<TSchema extends MongoDocument>(
-  transform: <TItem extends Document = Document> (item: TItem | null) => TItem
+  transform: <TItem extends Document> (item: TItem | null) => TItem
 ): (result: MongoDocument<TSchema> | MongoDocument<TSchema>[]) => TSchema | TSchema[] | null
 
-export declare function toModel<TSchema extends Document = Document>(doc: WithId<TSchema>): TSchema | null;
+export declare function toModel<TSchema extends Document>(doc: WithId<TSchema> | null): TSchema | null;
 
-export declare function toDoc<TSchema extends Document = Document>(model: TSchema): WithId<TSchema> | null;
+export declare function toDoc<TSchema extends Document>(model: TSchema): WithId<TSchema>;
