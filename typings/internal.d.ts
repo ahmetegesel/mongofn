b131b18d7a2d90c60e3aeab42aeeb0ca04bfa1ce
mert.kilic <mwert09@gmail.com>
2023-08-07 13:42:19 +0300

    refactor: add toModel overloads

--- typings/internal.d.ts
+++ typings/internal.d.ts
@@ -9,8 +9,8 @@ export declare function mapResultWith<TSchema extends MongoDocument>(
   transform: <TItem extends Document> (item: TItem | null) => TItem
 ): (result: MongoDocument<TSchema> | MongoDocument<TSchema>[]) => TSchema | TSchema[] | null
 
-export declare function toModel<TSchema>(doc: WithId<TSchema> | null): TSchema | null;
-export declare function toModel<TSchema>(doc: Array<WithId<TSchema>> | null): Array<TSchema> | null;
-export declare function toModel<TSchema>(doc: TSchema | null): TSchema | null;
+export declare function toModel<TSchema extends Document>(doc: WithId<TSchema> | null): TSchema | null;
+export declare function toModel<TSchema extends Document>(doc: Array<WithId<TSchema>> | null): Array<TSchema> | null;
+export declare function toModel<TSchema extends Document>(doc: TSchema | null): TSchema | null;
 
 export declare function toDoc<TSchema extends Document>(model: TSchema): WithId<TSchema>;
