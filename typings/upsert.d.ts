5e6b432283ffa5700b9337478b7f4d7b98485ae5
mert.kilic <mwert09@gmail.com>
2023-08-08 17:08:08 +0300

    refactor: change typings

--- typings/upsert.d.ts
+++ typings/upsert.d.ts
@@ -1,12 +1,12 @@
-import {Document, OptionalUnlessRequiredId} from "mongodb";
+import {OptionalUnlessRequiredId, WithId} from "mongodb";
 import {MongoClientLike} from "./client";
 
-type UpsertDocument<TSchema extends Document> = OptionalUnlessRequiredId<TSchema> | TSchema
+type UpsertDocument<TSchema> = OptionalUnlessRequiredId<TSchema> | TSchema
 
-export declare function upsert<TSchema extends Document>(client: MongoClientLike, databaseName: string, collectionName: string, doc: UpsertDocument): Promise<TSchema>;
-export declare function upsert<TSchema extends Document>(client: MongoClientLike, databaseName: string, collectionName: string): (doc: UpsertDocument) => Promise<TSchema>;
-export declare function upsert<TSchema extends Document>(client: MongoClientLike, databaseName: string): (collectionName: string) => (doc: UpsertDocument) => Promise<TSchema>;
-export declare function upsert<TSchema extends Document>(client: MongoClientLike, databaseName: string): (collectionName: string, doc: UpsertDocument) => Promise<TSchema>;
-export declare function upsert<TSchema extends Document>(client: MongoClientLike): (databaseName: string) => (collectionName: string) => (doc: UpsertDocument) => Promise<TSchema>;
+export declare function upsert<TSchema>(client: MongoClientLike, databaseName: string, collectionName: string, doc: UpsertDocument<TSchema>): Promise<WithId<TSchema>>;
+export declare function upsert<TSchema>(client: MongoClientLike, databaseName: string, collectionName: string): (doc: UpsertDocument<TSchema>) => Promise<WithId<TSchema>>;
+export declare function upsert<TSchema>(client: MongoClientLike, databaseName: string): (collectionName: string) => (doc: UpsertDocument<TSchema>) => Promise<WithId<TSchema>>;
+export declare function upsert<TSchema>(client: MongoClientLike, databaseName: string): (collectionName: string, doc: UpsertDocument<TSchema>) => Promise<WithId<TSchema>>;
+export declare function upsert<TSchema>(client: MongoClientLike): (databaseName: string) => (collectionName: string) => (doc: UpsertDocument<TSchema>) => Promise<WithId<TSchema>>;
 
 export default upsert;
