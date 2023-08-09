7fe9a9e8fa57ff43b5c374bb264da28e8781ebb7
mert.kilic <mwert09@gmail.com>
2023-08-09 17:01:02 +0300

    refactor: add types to useCollection

--- typings/useCollection.d.ts
+++ typings/useCollection.d.ts
@@ -1,9 +1,10 @@
 import {MongoClientLike} from "./client";
 import {Collection} from "mongodb";
 
-export declare function useCollection(client: MongoClientLike, databaseName: string, collectionName: string): Promise<Collection>
-export declare function useCollection(client: MongoClientLike, databaseName: string): (collectionName: string) => Promise<Collection>
-export declare function useCollection(client: MongoClientLike): (databaseName: string, collectionName: string) => Promise<Collection>
-export declare function useCollection(client: MongoClientLike): (databaseName: string) => (collectionName: string) => Promise<Collection>
+export declare function useCollection<TSchema>(client: MongoClientLike, databaseName: string, collectionName: string): Promise<Collection<TSchema>>
+export declare function useCollection<TSchema>(client: MongoClientLike, databaseName: string): (collectionName: string) => Promise<Collection<TSchema>>
+export declare function useCollection<TSchema>(client: MongoClientLike): (databaseName: string, collectionName: string) => Promise<Collection<TSchema>>
+export declare function useCollection<TSchema>(client: MongoClientLike): (databaseName: string) => (collectionName: string) => Promise<Collection<TSchema>>
+
 
 export default useCollection;
