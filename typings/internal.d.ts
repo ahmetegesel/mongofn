2ff536343d196a26fe6f8a7e065e56005ab2374e
mert.kilic <mwert09@gmail.com>
2023-08-10 18:13:09 +0300

    feat: add OptionalUnlessRequiredId

--- typings/internal.d.ts
+++ typings/internal.d.ts
@@ -1,4 +1,4 @@
-import {WithId} from "mongodb";
+import {EnhancedOmit, WithId} from "mongodb";
 
 export declare function mapResultWith<T>(
   transform: <TItem> (item: TItem | null) => TItem, result: T | T[]
@@ -7,8 +7,22 @@ export declare function mapResultWith<T>(
   transform: <TItem> (item: TItem | null) => TItem
 ): (result: T | T[]) => T | T[] | null
 
-export declare function toModel<TSchema>(doc: Array<WithId<TSchema>>): Array<TSchema>;
-export declare function toModel<TSchema>(doc: WithId<TSchema>): TSchema;
-export declare function toModel<TSchema>(doc: WithId<TSchema> | null): TSchema | null;
+export declare type InferModelIdType<TSchema> = TSchema extends {
+    id: infer IdType;
+} ? Record<any, never> extends IdType ? never : IdType : TSchema extends {
+    id?: infer IdType;
+} ? unknown extends IdType ? string : IdType : string;
 
-export declare function toDoc<TSchema>(model: TSchema): WithId<TSchema>;
+export declare type OptionalId<TSchema> = EnhancedOmit<TSchema, 'id'> & {
+    id?: InferModelIdType<TSchema>;
+};
+
+export declare type OptionalUnlessRequiredId<TSchema> = TSchema extends {
+    id: any;
+} ? TSchema : OptionalId<TSchema>;
+
+export declare function toModel<TSchema>(doc: Array<WithId<TSchema>>): Array<OptionalUnlessRequiredId<TSchema>>;
+export declare function toModel<TSchema>(doc: WithId<TSchema>): OptionalUnlessRequiredId<TSchema>;
+export declare function toModel<TSchema>(doc: WithId<TSchema> | null): OptionalUnlessRequiredId<TSchema> | null;
+
+export declare function toDoc<T, TSchema>(model: OptionalUnlessRequiredId<T>): WithId<TSchema>;
