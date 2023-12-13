7d8a20f0211816024288e15038fcbce2accde28f
mert.kilic <mwert09@gmail.com>
2023-12-13 15:45:11 +0300

    feat: add union support
    Added union support to toModel and
    toModelWithCount functions.

--- typings/internal.d.ts
+++ typings/internal.d.ts
@@ -31,14 +31,21 @@ export declare type WithModelId<TSchema> = EnhancedOmit<TSchema, '_id'> & {
     id: InferModelIdType<TSchema>;
 };
 
-export declare function toModel<TSchema>(doc: Array<WithId<TSchema>>): Array<WithModelId<TSchema>>;
-export declare function toModel<TSchema>(doc: Array<WithId<TSchema>> | null): Array<WithModelId<TSchema>> | null;
-export declare function toModel<TSchema>(doc: WithId<TSchema>): WithModelId<TSchema>;
-export declare function toModel<TSchema>(doc: WithId<TSchema> | null): WithModelId<TSchema> | null;
+export declare type DistributeModelIdOverUnions<T> = T extends any ? WithModelId<T> : never;
+
+type IsUnion<T, U extends T = T> =
+    T extends unknown ? [U] extends [T] ? false : true : false;
+
+export declare function toModel<TSchema>(doc: Array<WithId<TSchema>>): IsUnion<TSchema> extends true ? Array<DistributeModelIdOverUnions<TSchema>> : Array<WithModelId<TSchema>>;
+export declare function toModel<TSchema>(doc: Array<WithId<TSchema>> | null): IsUnion<TSchema> extends true ? Array<DistributeModelIdOverUnions<TSchema>> :Array<WithModelId<TSchema>> | null;
+export declare function toModel<TSchema>(doc: WithId<TSchema>): IsUnion<TSchema> extends true ? DistributeModelIdOverUnions<TSchema> : WithModelId<TSchema>;
+export declare function toModel<TSchema>(doc: WithId<TSchema> | null):IsUnion<TSchema> extends true ? DistributeModelIdOverUnions<TSchema> : WithModelId<TSchema> | null;
 
 export declare function toDoc<T>(model: WithModelId<T>): WithId<T>;
 export declare function toDoc<T>(model: OptionalUnlessRequiredId<T>): WithId<T>;
 
 export declare function isResultWithoutCount<T>(value: T[] | [T[], number]): value is T[]
 
-export declare function toModelWithCount<T>(value: WithId<T>[] | [WithId<T>[], number]): WithModelId<T>[] | [WithModelId<T>[], number];
+export declare function toModelWithCount<T>(value: WithId<T>[] | [WithId<T>[], number]): IsUnion<T> extends true ?
+    DistributeModelIdOverUnions<T>[] | [DistributeModelIdOverUnions<T>[], number] :
+    WithModelId<T>[] | [WithModelId<T>[], number];
